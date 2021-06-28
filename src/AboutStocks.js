270dee758c24b8142c0c4f9a0d247bb6c9317d87
Abhishek kumar <abhishek1122338@gmail.com>
2021-06-28 11:42:09 +0530

    Initial Commit

---
+++ src/AboutStocks.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function AboutStocks() {
+    return (
+        <div>
+            <h1 className="Headingstyle">Top 
+            Stocks Exchanges in The world</h1>
+            <div className="Headingstyle2">
+            <h3>1.New York Stock Exchange (NYSE) </h3>
+            <h3>2. NASDAQ </h3>
+            <h3>3. Tokyo Stock Exchange </h3>
+            <h3>4. Shanghai Stock Exchange (SSE) </h3>
+        
+            </div>
+            
+        </div>
+    )
+}
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Stock from './Stock';
+import './App.css';
+import AboutStocks from './AboutStocks';
+
+function App() {
+  return (
+    <div className="App">
+      <Stock></Stock>
+      <AboutStocks />
+    </div>
+  );
+}
+
+export default App;
